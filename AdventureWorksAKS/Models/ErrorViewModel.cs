f7d2e0b142c64ff160bd9ac6f99de0731c86101f
Dennis Moon <dmoon396@hotmail.com>
2019-02-15 18:10:39 -0600

    Add project files.

---
+++ AdventureWorksAKS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AdventureWorksAKS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
